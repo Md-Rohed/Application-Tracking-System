87a4488e8f22836eaa9d04400f9c67de5eaff02f
Rohed <mdrohed85@gmail.com>
2025-11-03 16:40:16 +0600

    Jobdetails page added

--- ats-frontend/app/job/[id]/page.js
+++ ats-frontend/app/job/[id]/page.js
@@ -1,9 +1,16 @@
 "use client";
 import React from "react";
 import JobDetails from "@/app/components/JobDetails";
+import { getJobsById } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Job({ params }) {
     const { id } = React.use(params);
 
-    return <JobDetails jobId={id} />;
+    const { data: job, isLoading, error } = useQuery({
+        queryKey: ["job", id],
+        queryFn: () => getJobsById(id),
+    });
+
+    return <JobDetails job={job} isLoading={isLoading} error={error} jobId={id} />;
 }
